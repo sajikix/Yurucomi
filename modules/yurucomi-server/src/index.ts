4589ddc83b1c57cc190a6cbc0da2efb2632b98f2
saji-ryu <rsajiki@gmail.com>
2018-10-22 09:44:33 +0900

    enable socket.io

--- modules/yurucomi-server/src/index.ts
+++ modules/yurucomi-server/src/index.ts
@@ -2,6 +2,9 @@ import express from "express";
 import _debug from "debug";
 import dotenv from "dotenv";
 import cors from "cors";
+import bodyParser from "body-parser";
+import { createServer, Server } from "http";
+import socketIO from "socket.io";
 
 const debug = _debug("server:main");
 dotenv.load();
@@ -9,15 +12,27 @@ dotenv.load();
 const main = async () => {
   const PORT: number = Number(process.env.PORT) || 3000;
   const app = express();
+  const server: Server = createServer(app);
+  const io: SocketIO.Server = socketIO.listen(server);
+  app.set("views", "views/");
+  app.set("view engine", "pug");
+  app.use(express.static("public/"));
+  app.use(bodyParser.json());
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  );
   app.use(cors());
   app.use(express.static("public"));
 
-  app.listen(PORT, async () => {
-    debug(`Yurucomi http server listen on :${PORT}`);
+  server.listen(PORT, async () => {
+    debug(`Yurucomi server listen on :${PORT}`);
     debug(`process.env.NODE_ENV=${String(process.env.NODE_ENV)}`);
   });
 };
 
 main().catch(e => {
+  debug(`error:${e}`);
   process.exit(1);
 });
